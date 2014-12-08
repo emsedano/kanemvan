5ece1b746d32c8f5fab7c0eaecbc21d5608c747f
Elias Martinez <emsedano@live.com.mx>
2014-12-08 22:03:28 +0000

    login page V1

--- client/user/user_login.js
+++ client/user/user_login.js
@@ -4,9 +4,9 @@ Template.user_loggedout.events({
       requestPermissions: ['user', 'public_repo']
     }, function(err){
       if (err) {
-        // who message     
+        alert(err); // some custom packages
       } else{
-        // show alert
+        Router.go('dashboard');
       }
     })
   },
@@ -28,11 +28,12 @@ Template.user_loggedout.events({
           // The user might not have been found, or their passwword
           // could be incorrect. Inform the user that their
           // login attempt has failed. 
-          alert(err);
+          
+          Router.go('signin');
         } else{
           // The user has been logged in.
-          alert("logged in");
-          }
+          Router.go('dashboard');
+        }
       });
          return false; 
       }
@@ -43,10 +44,54 @@ Template.user_logged.events({
   "click #logout": function(e, tmpl){
     Meteor.logout(function(err){
       if(err){
-        
+        alert(err);
       }else{
-        
+        Router.go('signin');
+      }
+    });
+    return false;
+  }
+});
+
+
+
+Template.signin.events({
+  "click #login": function(e, tmpl){
+    Meteor.loginWithGithub({
+      requestPermissions: ['user', 'public_repo']
+    }, function(err){
+      if (err) {
+        alert(err); // some custom packages
+      } else{
+        Router.go('dashboard');
       }
     })
-  }
+  },
+  
+  
+  'submit #login-form' : function(e, tmpl){
+      
+      e.preventDefault();
+      // retrieve the input field values
+      var email = tmpl.find('#username').value
+        , password = tmpl.find('#password').value;
+
+        // Trim and validate your fields here.... 
+
+        // If validation passes, supply the appropriate fields to the
+        // Meteor.loginWithPassword() function.
+        Meteor.loginWithPassword(email, password, function(err){
+        if (err){
+          // The user might not have been found, or their passwword
+          // could be incorrect. Inform the user that their
+          // login attempt has failed. 
+          
+          Router.go('signin');
+        } else{
+          // The user has been logged in.
+          Router.go('dashboard');
+        }
+      });
+         return false; 
+      }
 });
